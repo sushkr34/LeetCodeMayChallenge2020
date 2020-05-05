5c9c311665e1f51ee9af36bfed3639b7c56e070d
Sushant Kumar <36587557+sushkr34@users.noreply.github.com>
2020-05-05 18:38:28 +0530

    Add files via upload

---
+++ first_unique_character.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int firstUniqChar(String s) {
+       HashMap<Character,Integer> scoreboard = new HashMap<>(); 
+        char ch='\0';
+        boolean flag=false;
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i); 
+        if (scoreboard.containsKey(c)) {
+            scoreboard.put(c, scoreboard.get(c) + 1);
+        } else { 
+            scoreboard.put(c, 1); 
+        }
+    } 
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i); 
+            if (scoreboard.get(c) == 1) {
+              ch=c;
+                flag=true;
+                break;
+                
+            } 
+        }
+        if (flag==true){
+            return ( s.indexOf(ch));
+        }
+        return -1;
+    
+
+    }
+}
